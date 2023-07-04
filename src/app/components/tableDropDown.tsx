19016a229721086effb10f56e5d887bcff251cdc
lawblaze <lawblaze4@gmail.com>
2023-07-04 16:49:43 +0100

    made some changes to the dropdown

--- src/app/components/tableDropDown.tsx
+++ src/app/components/tableDropDown.tsx
@@ -1,18 +1,49 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export const TableDropDown = ({ action }: any) => {
   const [openDropDown, setOpenDropDown] = useState(false);
+  const dropdownRef:any = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target)
+      ) {
+        setOpenDropDown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
-     <button onClick={() => setOpenDropDown(!openDropDown)}>
-          {!openDropDown ? "open" : "close"}
-        </button>
-      {openDropDown && <ul className="flex flex-col gap-2 p-absolute shadow-lg bg-white p-2 rounded-md absolute w-[200px] lg:ml-[75px] md:ml-[auto] ml-[auto]">
-        {
-          action?.type.map((a: any, i: number) => {
-            return <li key={i} className={`${a == "delete" ? 'text-red-500' : "text-green-500"} capitalize`}>{a}</li>;
+      <button onClick={() => setOpenDropDown(!openDropDown)}>
+        {!openDropDown ? "Open" : "Close"}
+      </button>
+      {openDropDown && (
+        <ul
+          ref={dropdownRef}
+          className="flex flex-col gap-2 absolute z-10 shadow-lg bg-white p-2 rounded-md w-[200px] lg:ml-[75px] md:ml-[auto] ml-[auto]"
+        >
+          {action?.type.map((a: any, i: number) => {
+            return (
+              <li
+                key={i}
+                className={`${a === "delete" ? "text-red-500" : "text-green-500"} capitalize`}
+                onClick={() => setOpenDropDown(false)}
+              >
+                {a}
+              </li>
+            );
           })}
-      </ul>}
+        </ul>
+      )}
     </>
   );
 };
